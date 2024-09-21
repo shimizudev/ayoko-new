8dc4df43ad1f6b8ff4a2648ee62d34f47af1269f
Mohtasim Alam Sohom <94981761+devaoto@users.noreply.github.com>
2024-09-21 21:41:58 +0600

    Create useMounted hook

---
+++ hooks/use-mounted.ts
@@ -0,0 +1,13 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+
+export default function useMounted(): boolean {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  return mounted;
+}
