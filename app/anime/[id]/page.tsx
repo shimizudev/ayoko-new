862d5a858f2f185f11657ac94b524712e3f01099
Mohtasim Alam Sohom <94981761+devaoto@users.noreply.github.com>
2024-09-23 09:19:26 +0600

    Create the anime info page

---
+++ app/anime/[id]/page.tsx
@@ -0,0 +1,11 @@
+import AnimeInfoContainer from '@/components/info/container';
+import { getAnimeInfo } from '@/lib/fetch/anime';
+
+export default async function Page({
+  params,
+}: {
+  params: { id: string };
+}): Promise<JSX.Element> {
+  const anime = await getAnimeInfo(params.id);
+  return <AnimeInfoContainer anime={anime} />;
+}
