889a0e0ddd265a499ecfc9d98db86cdea677ed2c
Mohtasim Alam Sohom <94981761+devaoto@users.noreply.github.com>
2024-09-25 23:33:54 +0600

    Add utility function to get month name

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import { MONTH_INDEX_OFFSET } from './constants';
 
 export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs));
@@ -13,4 +14,29 @@ export function getSeason(month: number): string {
     return 'SUMMER';
   }
   return month >= 9 && month <= 11 ? 'FALL' : 'WINTER';
+}
+
+export function getMonthName(monthNumber: number): string {
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
+  const adjustedMonthNumber = monthNumber - MONTH_INDEX_OFFSET;
+
+  if (adjustedMonthNumber < 0 || adjustedMonthNumber >= months.length) {
+    return 'Invalid month number';
+  }
+
+  return months[adjustedMonthNumber];
 }
