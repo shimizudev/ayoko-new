d15dacd84dc3416c7aafdc96d3d0ae54be6c003d
Mohtasim Alam Sohom <94981761+devaoto@users.noreply.github.com>
2024-09-30 22:13:50 +0600

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,2 +1,6 @@
 export const ANIFY_URL = 'https://anify.eltik.cc';
 export const MONTH_INDEX_OFFSET = 1;
+export const BASE_URL =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3000'
+    : process.env.URL;
